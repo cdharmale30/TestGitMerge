4111da8240dbc9b22cb4cb6ea698e654fbe8004d
Chaitanya Dharmale <cdharmale30@gmail.com>
2019-02-17 22:52:54 -0800

    testingGit

---
+++ src/main/java/com/test/multiwindows/MergeTest.java
@@ -0,0 +1,8 @@
+package com.test.multiwindows;
+
+public class MergeTest {
+	public static void main(String[] args) {
+		System.out.println(12345678);
+	}
+
+}
--- src/main/java/com/test/multiwindows/TestBranch.java
+++ src/main/java/com/test/multiwindows/TestBranch.java
@@ -3,7 +3,8 @@ package com.test.multiwindows;
 public class TestBranch {
 	public static void main(String[] args) {
 		
-		System.out.println(123456);
+		System.out.println("from test Branch");
+		System.out.println("from test Branch");
 		
 	}
 
